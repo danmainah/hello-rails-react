b248df172980e87453952eb403b8cbfdaca59497
danmainah <danielmainawaweru@gmail.com>
2021-11-09 23:02:40 +0300

    Update app component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,20 +1,17 @@
-import React from "react"
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Greeting from './Greeting';
-import PropTypes from "prop-types"
-class App extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-        <BrowserRouter>
-           <Routes>
-            <Route path="/" element={<Greeting />} />
-            <Route path="/greet" element={<Greeting greeting= {'hello'} />} />
-          </Routes>
-        </BrowserRouter>
-      </React.Fragment>
-    );
-  }
-}
+import store from '../redux/store';
 
-export default App
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+);
+
+export default App;
